5ac125c50b115032b61e8e4faff1d7bdeabf11ab
amalyutin <33480745+amalyutin@users.noreply.github.com>
2017-11-13 22:03:52 +0300

    Initial commit

---
+++ src/main/java/MoveWindowMain.java
@@ -0,0 +1,8 @@
+import jFrame.MoveFigureWindow;
+
+public class MoveWindowMain {
+    public static void main(String[] args){
+        MoveFigureWindow window = new MoveFigureWindow();
+        window.setVisible(true);
+    }
+}
---
+++ src/main/java/SimpleWindowMain.java
@@ -0,0 +1,13 @@
+import jFrame.Window;
+
+/**
+ * Created by User on 07.11.2017.
+ *
+ */
+public class SimpleWindowMain {
+
+    public static void main(String[] args){
+        Window window = new Window();
+        window.setVisible(true);
+    }
+}
